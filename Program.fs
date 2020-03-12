9683899b9331f525049e5a121f4a5e11b751e1bd
sritharans <48209027+sritharans@users.noreply.github.com>
2020-03-13 06:44:47 +0800

    Initial commit
    Initial commit containing base project files and instructions

---
+++ Program.fs
@@ -0,0 +1,66 @@
+﻿//////////////////////////////////////////////////////////////////////////////////////////////////////////
+// About: This is a web scraping tool to download an AliExpress item listing and
+//        extract similar listings to the item from other AliExpress merchants.
+//        The output contains the description, URL and pricing of other similar listings.
+//        The data could then be imported to perform price comparisons and other analysis.
+//
+// Authors: Sritharan Sivaguru (WQD180086)
+//          Mohamed Fathi Mohamed (WQD180081)
+//
+// Learn more about F# at http://fsharp.org
+
+open System
+open PuppeteerSharp
+open FSharp.Data
+
+// CSV Schema that will be used for output later
+type ProductInfo = CsvProvider<Sample = "Description, URL, Price",
+                               Schema = "string, string, string",
+                               HasHeaders=true>
+
+// Get the HTNL data from an AliExpress listing
+let GetAEListing url = async {
+    // Download the Puppeteer Chrome browser
+    do! BrowserFetcher().DownloadAsync BrowserFetcher.DefaultRevision |> Async.AwaitTask |> Async.Ignore
+
+    // Set the browser to be headless (invisible)
+    let options = LaunchOptions ()
+    options.Headless <- true
+    
+    // Simulate user browsing activity to load the similar items listing
+    use! browser = Puppeteer.LaunchAsync options |> Async.AwaitTask
+    use! page = browser.NewPageAsync () |> Async.AwaitTask
+    do! page.GoToAsync (url, WaitUntilNavigation.Networkidle0) |> Async.AwaitTask |> Async.Ignore
+    do! page.ClickAsync ("#root") |> Async.AwaitTask |> Async.Ignore
+    do! page.EvaluateExpressionAsync "window.scrollBy(0, document.body.scrollHeight);" |> Async.AwaitTask |> Async.Ignore
+    do! page.WaitForSelectorAsync ".other-store-more" |> Async.AwaitTask |> Async.Ignore
+    do! page.HoverAsync ".other-store-more" |> Async.AwaitTask |> Async.Ignore
+
+    // Return the HTML data captured by the browser
+    return! page.GetContentAsync () |> Async.AwaitTask
+}
+
+[<EntryPoint>]
+let main argv =
+    match argv.Length with
+    | 2 -> printfn "\n\tDownloading AliExpress listing data:"
+           printfn "\t%s" argv.[0]
+           let output = GetAEListing argv.[0] |> Async.RunSynchronously
+           // Parse the HTML data and filter out unwanted tags
+           printfn "\tParsing HTML for Item listings..."
+           let doc = HtmlDocument.Parse(output)
+           let imore = doc.CssSelect("div.other-store-more div.item-info a")
+           let pmore = doc.CssSelect("div.other-store-more div.item-info div.item-price span")
+           // Parse the filtered HTML tags and extract the related item data
+           printfn "\tGenerating CSV data of item listing..."
+           let pinfo = new ProductInfo((imore, pmore)
+           ||> Seq.map2 (fun i p ->
+                let ia = "https:" + i.AttributeValue("href")
+                let pi = p.InnerText().Trim()
+                (ProductInfo.Row(i.InnerText().Trim(), ia.Remove(ia.IndexOf('?')), pi.Substring(pi.IndexOf('$')+1)))))
+           // Save the generated CSV output into a file
+           printfn "\tSaving output into: %s" argv.[1]
+           pinfo.Save(argv.[1])
+    | _ -> printfn "\tURL and CSV file name required."
+           printfn "\tExample: dotnet run https://www.aliexpress.com/item/xxx.html output.csv"
+    0 // return an integer exit code
---
+++ README.md
@@ -0,0 +1,29 @@
+# Welcome to the AE Listing Scrapper!
+
+This is a simple tool written in F# using .Net Core 3.1.
+This tool is dependent on the following 3rd party components:
+
+ - [Puppeteer Sharp](https://www.puppeteersharp.com)
+ - [F# Data](http://fsharp.github.io/FSharp.Data/)
+
+# Build Instructions
+
+> **Note:** You will require an Internet connection to build the program and run it. Web proxies are not supported, a direct Internet connection is required.
+
+Steps:
+
+ 1. Download and install the .Net Core SDK v3.1 from [here.](https://dotnet.microsoft.com/download)
+ 2. Clone this Github repository or download it using **Clone or download > Download Zip** and extract it to your hard drive.
+ 3. Using the command line, switch to the cloned repository or extracted folder and run the following command:`dotnet restore`
+ 4. After a successful restore then run the following command: `dotnet build`
+ 5. Then finally run the program using: `dotnet run`
+ 6. Follow the instructions to generate the listing data using the program, like this:`dotnet run https://www.aliexpress.com/item/4000478431034.html output.csv`
+
+## To view the CSV
+
+Open the generated CSV file in any text editor of your choice or compatible spreadsheet application.
+
+### Authors
+
+ - Sritharan Sivaguru
+ - Mohamed Fathi Mohamed
